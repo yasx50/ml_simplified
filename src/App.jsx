e19e5d8c4cbd806437ebc27f72b09f36960ce97b
yasx50 <yasxcode@gmail.com>
2025-07-09 14:34:33 +0530

    design changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
-import MLSimplifiedLanding from "./components/Home";
+
 // import User from "./components/User";
 
 
 const App = () => {
   return (
     <>
-     <Routes>
-         
-            <Route path="/" element={ <MLSimplifiedLanding/>} />
-            {/* <Route path="/profile" element={<User/>} /> */}
-           
-         </Routes>
       </>
     
   );
